4de9c4d4658532bc99ca8b5d73a1aa43b224e231
djoseitz <72769693+djoseitz@users.noreply.github.com>
2020-10-20 10:56:44 -0700

    displayed message if pokemon > 2m high

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,36 @@
 let pokemonList = [
-    {name: 'Pikachu', height: 1.04, type: 'Electric'},
-    {name: 'Bulbasaur', height: 2.04, type: 'Grass'},
-    {name: 'Charmander', height: 2, type: 'Fire'},
-    {name: 'Squirtle', height: 1.08, type: 'Water'}
+    {
+        name: 'Pikachu', 
+        height: 1.04, 
+        type: 'Electric'
+    },
+    {
+        name: 'Bulbasaur', 
+        height: 2.04, 
+        type: ['Grass', 'poison']
+    },
+    {
+        name: 'Charmander', 
+        height: 2, 
+        type: 'Fire'
+    },
+    {
+        name: 'Squirtle', 
+        height: 1.08, 
+        type: 'Water'
+    }
 ];
 
+//Lists pokemon
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ") ");
-}
+    //displays "Wow that's big!" if pokemon over 2m high
+    if (pokemonList[i].height > 2) {
+        document.write(pokemonList[i].name + 
+            " (height: " + pokemonList[i].height + ") - Wow that's big!");
+        }
+    //displays rest of pokemon without "Wow that's big!" message
+    else {
+        document.write(pokemonList[i].name + 
+            " (height: " + pokemonList[i].height + ")");
+        }
+    }
