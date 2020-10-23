be7568feabec0c63c9d19105ed4b310cf2d1b8c0
djoseitz <72769693+djoseitz@users.noreply.github.com>
2020-10-23 13:05:32 -0700

    wrapped list in IIFE and added conditional back to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,49 @@
-//Pokemon list
-let pokemonList = [
-    {
-        name: 'Pikachu', 
-        height: 1.04, 
-        type: 'Electric'
-    },
-    {
-        name: 'Bulbasaur', 
-        height: 2.04, 
-        type: ['Grass', 'poison']
-    },
-    {
-        name: 'Charmander', 
-        height: 2, 
-        type: 'Fire'
-    },
-    {
-        name: 'Squirtle', 
-        height: 1.08, 
-        type: 'Water'
-    }
-];
+//Pokemon index
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Pikachu', 
+            height: 1.04, 
+            type: 'Electric'
+        },
+        {
+            name: 'Bulbasaur', 
+            height: 2.04, 
+            type: ['Grass', 'poison']
+        },
+        {
+            name: 'Charmander', 
+            height: 2, 
+            type: 'Fire'
+        },
+        {
+            name: 'Squirtle', 
+            height: 1.08, 
+            type: 'Water'
+        }
+    ];
 
-pokemonList.forEach(function(pokemon) {
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    if (pokemon.height > 2) {
+        document.write(`<div class="pokemon">${pokemon.name}  
+            (${pokemon.height}m) - Wow that's big!</div>` )
+    }
+    else {
         document.write(`<div class="pokemon">${pokemon.name}  
         (${pokemon.height}m)</div>` )
+    }
 })
