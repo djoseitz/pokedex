0ada8f53b3f5df98c64059dac800c37107293fd9
djoseitz <72769693+djoseitz@users.noreply.github.com>
2020-10-26 13:04:52 -0700

    updated list to be buttons and use addListItem function

--- js/scripts.js
+++ js/scripts.js
@@ -36,12 +36,28 @@ let pokemonRepository = (function () {
         }
     }
 
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listitem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('pokebutton');
+        listitem.appendChild(button);
+        pokemonList.appendChild(listitem);
+    }
+
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        addListItem: addListItem
     };
 })();
 
+pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+})
+
+/*
 pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height > 2) {
         document.write(`<div class="pokemon">${pokemon.name}  
@@ -52,3 +68,4 @@ pokemonRepository.getAll().forEach(function(pokemon) {
         (${pokemon.height}m)</div>` )
     }
 })
+*/
